385b418f6f50bd91f46afb4c1e7efab4d296b8f4
PatrycjaWiza <patrycja.wiza@gmail.com>
2022-05-21 16:15:08 +0200

    added gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,40 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+// evt delegation
+gallery.addEventListener("click", selectImg);
+function selectImg(e) {
+  e.preventDefault();
+  //   if (e.target.nodeName !== "A") {
+  //     return;
+  //   }
+  const selectedImg = e.target.dataset.source;
+  const output = e.target;
+  output.src = selectedImg;
+  const instance = basicLightbox.create(output);
+
+  //   output.src = selectedImg;
+  //   const instance = basicLightbox.create(output);
+  instance.show();
+}
+
+// rendering gallery
+createGallery();
+function createGallery() {
+  const imgs = [];
+  galleryItems.forEach((e) => {
+    const { preview, original, description } = e;
+    const item = document.createElement("a");
+    item.classList.add("gallery__link");
+    item.href = `${original}`;
+    imgs.push(item);
+    const img = document.createElement("img");
+    img.classList.add("gallery__image");
+    img.src = `${preview}`;
+    img.dataset.source = `${original}`;
+    img.alt = `${description}`;
+    item.append(img);
+  });
+  gallery.append(...imgs);
+}
