03f6096d8cc69ef6b7d30566f4a862d9b3559cb4
PatrycjaWiza <patrycja.wiza@gmail.com>
2022-05-22 12:52:50 +0200

    task 02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+//render gallery
+createGallery();
+function createGallery() {
+  const imgs = [];
+  galleryItems.forEach((e) => {
+    const { preview, original, description } = e;
+    const item = document.createElement("a");
+    item.classList.add("gallery__link");
+    item.href = `${original}`;
+    imgs.push(item);
+    const img = document.createElement("img");
+    img.classList.add("gallery__image");
+    img.src = `${preview}`;
+    img.alt = `${description}`;
+    item.append(img);
+  });
+  gallery.append(...imgs);
+}
+
+//SimpleLightbox
+
+new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+  captionsData: "alt",
+});
